17dca04e11d48cf9720979324a5baa02134c183c
Shigehiro Fukase <shigehiro.fukase@gmail.com>
2023-12-02 16:08:21 +0900

    mod-psci

---
+++ Makefile
@@ -0,0 +1,28 @@
+# KERNEL_HEADERS=/lib/modules/$(shell uname -r)/build
+KERNEL_HEADERS=/usr/src/linux-headers-$(shell uname -r)
+
+CFILES = driver.c
+obj-m := psci-kick.o
+psci-kick-objs := $(CFILES:.c=.o)
+
+ccflags-y += -std=gnu99 -Wall -Wno-declaration-after-statement
+ccflags-y += -DDRIVER_NAME=\"$(obj-m:.o=)\"
+
+all:
+	$(MAKE) -C $(KERNEL_HEADERS) M=$(PWD) modules
+clean:
+	$(MAKE) -C $(KERNEL_HEADERS) M=$(PWD) clean
+
+.PHONY: rmmod insmod
+rmmod: all
+	-sudo rmmod $(obj-m:.o=)
+	-lsmod | grep $(obj-m:.o=)
+
+insmod: rmmod
+	-modprobe $(obj-m:.o=)
+	sudo insmod $(obj-m:.o=.ko) gpo=42 nsec=200000000 verbose=false
+	grep "" -r /sys/module/gpio_blinker/parameters/
+
+test:
+	raspi-gpio set 42 op pn
+	raspi-gpio get 42
---
+++ driver.c
@@ -0,0 +1,154 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/types.h>
+#include <linux/kernel.h>
+#include <linux/fs.h>
+#include <linux/cdev.h>
+#include <linux/sched.h>
+#include <linux/device.h>
+#include <linux/gpio.h>
+#include <linux/interrupt.h>
+#include <linux/hrtimer.h>
+#include <linux/ktime.h>
+#include <linux/arm-smccc.h>
+// #include <linux/uapi/linux/psci.h>
+#include <linux/psci.h>
+#include <asm/uaccess.h>
+
+MODULE_LICENSE("Dual BSD/GPL");
+#ifndef DRIVER_NAME
+#define DRIVER_NAME "psci-kick"	/* /proc/devices, /proc/modules */
+#endif /* DRIVER_NAME */
+
+#define PSCI_FN32_BASE	0x84000000
+#define PSCI_64_OFF	0x40000000
+#define PSCI_FN64_BASE	(PSCI_FN32_BASE + PSCI_64_OFF)
+#if defined(PSCI32)
+#error "NOT SUPPORT"
+#else
+#define PSCI_FN_BASE	PSCI_FN64_BASE
+#endif
+#define PSCI_FN(n)			(PSCI_FN_BASE + (n))
+#define PSCI_FN_PSCI_VERSION		PSCI_FN(0)
+#define PSCI_FN_CPU_SUSPEND		PSCI_FN(1)
+#define PSCI_FN_CPU_OFF			PSCI_FN(2)
+#define PSCI_FN_CPU_ON			PSCI_FN(3)
+#define PSCI_FN_AFFINITY_INFO		PSCI_FN(4)
+#define PSCI_FN_MIGRATE			PSCI_FN(5)
+#define PSCI_FN_MIGRATE_INFO_TYPE	PSCI_FN(6)
+#define PSCI_FN_MIGRATE_INFO_UP_CPU	PSCI_FN(7)
+#define PSCI_FN_SYSTEM_OFF		PSCI_FN(8)
+#define PSCI_FN_SYSTEM_RESET		PSCI_FN(9)
+
+
+/* PSCI return values (copy from linux-kernel/include/uapi/linux/psci.h) */
+#define PSCI_RET_SUCCESS			0
+#define PSCI_RET_NOT_SUPPORTED			-1
+#define PSCI_RET_INVALID_PARAMS			-2
+#define PSCI_RET_DENIED				-3
+#define PSCI_RET_ALREADY_ON			-4
+#define PSCI_RET_ON_PENDING			-5
+#define PSCI_RET_INTERNAL_FAILURE		-6
+#define PSCI_RET_NOT_PRESENT			-7
+#define PSCI_RET_DISABLED			-8
+#define PSCI_RET_INVALID_ADDRESS		-9
+
+/* default value */
+
+static bool use_hvc = false;
+module_param(use_hvc, bool, S_IRUGO);
+MODULE_PARM_DESC(use_hvc, "use HVC (default off = use SMC)");
+
+
+static bool verbose = false;
+module_param(verbose, bool, S_IRUGO);
+MODULE_PARM_DESC(verbose, "(optional) verbose mode");
+
+static unsigned long function_id = PSCI_FN_CPU_ON;
+module_param(function_id, ulong, S_IRUGO);
+MODULE_PARM_DESC(function_id, "arm smccc a0");
+static unsigned long arg0 = 3;
+module_param(arg0, ulong, S_IRUGO);
+MODULE_PARM_DESC(arg0, "arm smccc a1");
+static unsigned long arg1 = 0;
+module_param(arg1, ulong, S_IRUGO);
+MODULE_PARM_DESC(arg1, "arm smccc a2");
+static unsigned long arg2 = 0;
+module_param(arg2, ulong, S_IRUGO);
+MODULE_PARM_DESC(arg2, "arm smccc a3");
+
+static int __init mod_init(void);
+static void __exit mod_exit(void);
+
+static __always_inline unsigned long
+invoke_psci_fn_hvc(unsigned long function_id,
+		unsigned long arg0, unsigned long arg1, unsigned long arg2) {
+	struct arm_smccc_res res;
+	arm_smccc_hvc(function_id, arg0, arg1, arg2, 0, 0, 0, 0, &res);
+	return res.a0;
+}
+static __always_inline unsigned long
+invoke_psci_fn_smc(unsigned long function_id,
+		unsigned long arg0, unsigned long arg1, unsigned long arg2) {
+	struct arm_smccc_res res;
+	arm_smccc_smc(function_id, arg0, arg1, arg2, 0, 0, 0, 0, &res);
+	return res.a0;
+}
+static unsigned long invoke_psci_fn(unsigned long function_id,
+		unsigned long arg0, unsigned long arg1, unsigned long arg2) {
+	if (use_hvc) {
+		return invoke_psci_fn_hvc(function_id, arg0, arg1, arg2);
+	} else {
+		return invoke_psci_fn_smc(function_id, arg0, arg1, arg2);
+	}
+}
+static void print_psci_retval(unsigned long retval) {
+	const char * s;
+	switch (retval) {
+		case PSCI_RET_SUCCESS:          s = "PSCI_RET_SUCCESS";          break;
+		case PSCI_RET_NOT_SUPPORTED:    s = "PSCI_RET_NOT_SUPPORTED";    break;
+		case PSCI_RET_INVALID_PARAMS:   s = "PSCI_RET_INVALID_PARAMS";   break;
+		case PSCI_RET_DENIED:           s = "PSCI_RET_DENIED";           break;
+		case PSCI_RET_ALREADY_ON:       s = "PSCI_RET_ALREADY_ON";       break;
+		case PSCI_RET_ON_PENDING:       s = "PSCI_RET_ON_PENDING";       break;
+		case PSCI_RET_INTERNAL_FAILURE: s = "PSCI_RET_INTERNAL_FAILURE"; break;
+		case PSCI_RET_NOT_PRESENT:      s = "PSCI_RET_NOT_PRESENT";      break;
+		case PSCI_RET_DISABLED:         s = "PSCI_RET_DISABLED";         break;
+		case PSCI_RET_INVALID_ADDRESS:  s = "PSCI_RET_INVALID_ADDRESS";  break;
+		default: s = "UNKNOWN"; break;
+	}
+	printk(DRIVER_NAME ": retvalue %lu \"%s\"\n", retval, s);
+}
+static unsigned long psci_cpu_on(unsigned long fn, unsigned long cpuid, unsigned long entry_point) {
+	unsigned long psci_ret;
+	psci_ret = invoke_psci_fn(fn, cpuid, entry_point, 0);
+	print_psci_retval(psci_ret);
+	return psci_ret;
+}
+
+
+static void print_settings(void) {
+	printk(DRIVER_NAME ": func_id=%lu arg0=%lu arg1=%lu arg2=%lu\n", function_id, arg0, arg1, arg2);
+}
+
+
+/* At load (insmod) */
+static int __init mod_init(void) {
+	unsigned long psci_ret;
+	printk(DRIVER_NAME ": mod_init\n");
+
+	print_settings();
+
+	psci_ret = psci_cpu_on(function_id, arg0, arg1);
+
+	printk(DRIVER_NAME ": insmod done.\n");
+	return 0;
+}
+
+/* At unload (rmmod) */
+static void __exit mod_exit(void) {
+	printk(DRIVER_NAME ": mod_exit\n");
+}
+
+module_init(mod_init);
+module_exit(mod_exit);
